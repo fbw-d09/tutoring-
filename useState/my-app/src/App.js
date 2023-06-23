ef4c01a3ce2a514bc750a6560d336fe3f6212286
imadteryaki <115640742+imadteryaki@users.noreply.github.com>
2023-06-23 15:44:57 +0200

    Add useState example

---
+++ useState/my-app/src/App.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react'
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+       <p>You clicked {count} times</p>
+        <button onClick={() => setCount(count + 1)}>
+         Click me
+      </button>
+    </div>
+  );
+}
+
+export default App;
