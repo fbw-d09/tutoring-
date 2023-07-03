c62b10aed58e1c4b83f3c7ae57e51df22e6a749a
hansi <juergen-weber93@web.de>
2023-07-03 15:09:21 +0200

    add test-spa-adv vorbereitung

---
+++ test-spa-ad/testvorbereitung/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Counter from './Counter';
+import MapComponent from './MapComponent';
+
+
+function App() {
+ 
+  return (
+    <div className="App">
+     <Counter />
+     <MapComponent />
+    </div>
+  );
+}
+
+export default App;
